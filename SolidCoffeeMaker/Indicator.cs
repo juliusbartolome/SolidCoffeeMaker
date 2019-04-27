b2cf4b788582572921bd72641aed6923b66b4938
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2019-04-28 02:48:10 +0800

    added indicator class with test

---
+++ SolidCoffeeMaker/Indicator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SolidCoffeeMaker
+{
+    public class Indicator : IObserver<BoilerSensorStatus>, IObserver<BrewButtonStatus>
+    {
+        private readonly IOnOffDevice indicatorDevice;
+        private bool hasWater;
+        private bool isBrewing;
+
+        public Indicator(IOnOffDevice indicatorDevice)
+        {
+            this.indicatorDevice = indicatorDevice;
+        }
+
+        public void OnCompleted() { }
+
+        public void OnError(Exception error) { }
+
+        public void OnNext(BrewButtonStatus value)
+        {
+            this.isBrewing = value == BrewButtonStatus.IsPushed && this.hasWater;
+            if (this.isBrewing && this.indicatorDevice.IsOn())
+                this.indicatorDevice.Off();
+        }
+
+        public void OnNext(BoilerSensorStatus value)
+        {
+            this.hasWater = value == BoilerSensorStatus.BoilerNotEmpty;
+            if (!this.hasWater && this.isBrewing)
+            {
+                this.indicatorDevice.On();
+                this.isBrewing = false;
+            }
+            else
+            {
+                // Do nothing 
+            }
+        }
+    }
+}
---
+++ SolidCoffeeMakerTests/IndicatorTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using SolidCoffeeMaker;
+using Xunit;
+
+namespace SolidCoffeeMakerTests
+{
+    public class IndicatorTests
+    {
+        private readonly Mock<IOnOffDevice> indicatorDeviceMock;
+        private readonly Indicator sut;
+
+        public IndicatorTests()
+        {
+            this.indicatorDeviceMock = new Mock<IOnOffDevice>();
+            this.sut = new Indicator(this.indicatorDeviceMock.Object);
+        }
+
+        [Fact(DisplayName = "When coffee maker is brewing and boiler's water becomes empty, the indicator light should turn on.")]
+        public void OnNext_WhenBrewingAndBoilerEmpty_ShouldTurnOnIndicator()
+        {
+            this.sut.OnNext(BoilerSensorStatus.BoilerNotEmpty);
+            this.sut.OnNext(BrewButtonStatus.IsPushed);
+            this.sut.OnNext(BoilerSensorStatus.BoilerEmpty);
+
+            this.indicatorDeviceMock.Verify(i => i.On(), Times.Once);
+        }
+
+        [Fact(DisplayName = "When indicator is turned on and a new brewing cycle starts, indicator light should turn off.")]
+        public void OnNext_WhenIndicatorIsOnAndBrewingCycleStartsAgain_ShouldTurnOffIndicator()
+        {
+            // First Brewing Cycle
+            this.sut.OnNext(BoilerSensorStatus.BoilerNotEmpty);
+            this.sut.OnNext(BrewButtonStatus.IsPushed);
+            this.sut.OnNext(BoilerSensorStatus.BoilerEmpty);
+            this.indicatorDeviceMock.Setup(i => i.IsOn()).Returns(true);
+            // Second Brewing Cycle
+            this.sut.OnNext(BoilerSensorStatus.BoilerNotEmpty);
+            this.sut.OnNext(BrewButtonStatus.IsPushed);
+
+            this.indicatorDeviceMock.Verify(i => i.Off(), Times.Once);
+        }
+    }
+}
