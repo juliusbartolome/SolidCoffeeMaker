83cd4d2d37872a5631251d1803fda5cf150a34ee
Julius Philip Bartolome <juliusbartolome@hotmail.ph>
2019-05-01 00:51:28 +0800

    added indicator component

---
+++ SolidCoffeeMaker/IndicatorComponent.cs
@@ -0,0 +1,34 @@
+﻿namespace SolidCoffeeMaker
+{
+    public class IndicatorComponent : ICoffeeMakerComponent
+    {
+        private readonly IOnOffDevice indicatorDevice;
+
+        public IndicatorComponent(IOnOffDevice indicatorDevice)
+        {
+            this.indicatorDevice = indicatorDevice;
+        }
+
+        public void EmptyBoilerWater()
+        {
+        }
+
+        public void FinishBrewing()
+        {
+            this.indicatorDevice.On();
+        }
+
+        public void InterruptBrewing()
+        {
+        }
+
+        public void RefillBoilerWater()
+        {
+        }
+
+        public void StartBrewing()
+        {
+            this.indicatorDevice.Off();
+        }
+    }
+}
---
+++ SolidCoffeeMakerTests/IndicatorComponentTests.cs
@@ -0,0 +1,52 @@
+﻿using Moq;
+using SolidCoffeeMaker;
+using Xunit;
+
+namespace SolidCoffeeMakerTests
+{
+    public class IndicatorComponentTests
+    {
+        private readonly Mock<IOnOffDevice> indicatorDeviceMock;
+        private readonly IndicatorComponent sut;
+
+        public IndicatorComponentTests()
+        {
+            this.indicatorDeviceMock = new Mock<IOnOffDevice>();
+            this.sut = new IndicatorComponent(this.indicatorDeviceMock.Object);
+        }
+
+        [Fact(DisplayName = "Empty Boiler should not throw a not implemented exception")]
+        public void EmptyBoilerWater_ShouldNot_Throw_NotImplementedException()
+        {
+            this.sut.EmptyBoilerWater();
+        }
+
+        [Fact(DisplayName = "Finish Brewing should turn indicator device on.")]
+        public void FinishBrewing_Should_TurnOn_IndicatorDevice()
+        {
+            this.sut.FinishBrewing();
+
+            this.indicatorDeviceMock.Verify(i => i.On(), Times.Once);
+        }
+
+        [Fact(DisplayName = "Interrupt Brewing should not throw a not implemented exception")]
+        public void InterruptBrewing_ShouldNot_Throw_NotImplementedException()
+        {
+            this.sut.InterruptBrewing();
+        }
+
+        [Fact(DisplayName = "Refill Boiler Water should not throw a not implemented exception")]
+        public void RefillBoilerWater_ShouldNot_Throw_NotImplementedException()
+        {
+            this.sut.RefillBoilerWater();
+        }
+
+        [Fact(DisplayName = "Start Brewing should turn indicator device off.")]
+        public void StartBrewing_Should_TurnOn_IndicatorDevice()
+        {
+            this.sut.StartBrewing();
+
+            this.indicatorDeviceMock.Verify(i => i.Off(), Times.Once);
+        }
+    }
+}
